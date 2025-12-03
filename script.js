d37c0686fae6a4ab065ba5208ecd8276d7e44e03
Aditi Inamdar <66082809+aditiinamdar@users.noreply.github.com>
2025-12-03 15:29:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ const counterElement = document.getElementById('counter');
+        const incrementButton = document.getElementById('incrementBtn');
+        let counter = 0;
+
+        incrementButton.addEventListener('click', function() {
+            alert(`Current value: ${counter}`);
+            counter++;
+            counterElement.textContent = counter;
+        });
